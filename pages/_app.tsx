592bb3248b8667c6d2cdcc61729b75ca35c0911a
daichi-sato-design <daichisato.design@gmail.com>
2021-10-05 23:56:19 +0900

    ApolloProviderの実装

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import { AppProps } from 'next/app'
+import { ApolloProvider } from '@apollo/client'
+import { initializeApollo } from '../lib/apolloClient'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const client = initializeApollo()
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 MyApp
