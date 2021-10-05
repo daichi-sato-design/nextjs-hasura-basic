83753c12bdfa826c31ee7ab001a1826c3e2d55c0
daichi-sato-design <daichisato.design@gmail.com>
2021-10-06 00:22:00 +0900

    fetchPolicyの追加

--- pages/hasura-main.tsx
+++ pages/hasura-main.tsx
@@ -6,7 +6,9 @@ import { GetUsersQuery } from '../types/generated/graphql'
 import { Layout } from '../components/Layout'
 
 const FetchMain: VFC = () => {
-  const { data, error } = useQuery<GetUsersQuery>(GET_USERS)
+  const { data, error } = useQuery<GetUsersQuery>(GET_USERS, {
+    fetchPolicy: 'network-only',
+  })
 
   if (error)
     return (
