e4d17099fcec43d64fcf56cdefb20b3536acbdf1
daichi-sato-design <daichisato.design@gmail.com>
2021-10-05 23:57:12 +0900

    apolloでhasuraと通信するページ作成

---
+++ pages/hasura-main.tsx
@@ -0,0 +1,33 @@
+import { VFC } from 'react'
+import Link from 'next/link'
+import { useQuery } from '@apollo/client'
+import { GET_USERS } from '../queries/queries'
+import { GetUsersQuery } from '../types/generated/graphql'
+import { Layout } from '../components/Layout'
+
+const FetchMain: VFC = () => {
+  const { data, error } = useQuery<GetUsersQuery>(GET_USERS)
+
+  if (error)
+    return (
+      <Layout title="Hasura fetchPolicy">
+        <p>Error: {error.message}</p>
+      </Layout>
+    )
+  return (
+    <Layout title="Hasura fetchPolicy">
+      <p className="mb-6 font-bold">Hasura main page</p>
+      {console.log(data)}
+      {data?.users.map((user, index) => (
+        <p className="my-1" key={index}>
+          {user.name}
+        </p>
+      ))}
+      <Link href="/hasura-sub">
+        <a className="mt-6">Next</a>
+      </Link>
+    </Layout>
+  )
+}
+
+export default FetchMain
