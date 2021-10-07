a136456770c688d62dfa805e8cadcb68b6d15a4b
daichi-sato-design <daichisato.design@gmail.com>
2021-10-07 23:35:42 +0900

    cachePolicyをコメントで全種類表示

--- pages/hasura-main.tsx
+++ pages/hasura-main.tsx
@@ -7,7 +7,10 @@ import { Layout } from '../components/Layout'
 
 const FetchMain: VFC = () => {
   const { data, error } = useQuery<GetUsersQuery>(GET_USERS, {
-    fetchPolicy: 'network-only',
+    // fetchPolicy: 'network-only',
+    fetchPolicy: 'cache-and-network',
+    // fetchPolicy: 'cache-first',
+    // fetchPolicy: 'cache-no-cache',
   })
 
   if (error)
