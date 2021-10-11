6fe0140ce7ab2f95f165e22fd1a35f5c981d1d51
daichi-sato-design <daichisato.design@gmail.com>
2021-10-11 21:31:04 +0900

    hasura-ssgページの作成

--- pages/hasura-ssg.tsx
+++ pages/hasura-ssg.tsx
@@ -6,6 +6,31 @@ import { GET_USERS } from '../queries/queries'
 import { GetUsersQuery, Users } from '../types/generated/graphql'
 import { Layout } from '../components/Layout'
 
+interface Props {
+  users: ({
+    __typename?: 'users'
+  } & Pick<Users, 'id' | 'name' | 'created_at'>)[]
+}
+
+const HasuraSSG: VFC<Props> = ({ users }) => {
+  return (
+    <Layout title="Hasura SSG">
+      <p className="mb-3 font-bold">SSG + ISR</p>
+      {users?.map((user) => {
+        return (
+          <Link key={user.id} href={`/users/${user.id}`}>
+            <a className="my-1 cursor-pointer" data-testid={`link-${user.id}`}>
+              {user.name}
+            </a>
+          </Link>
+        )
+      })}
+    </Layout>
+  )
+}
+
+export default HasuraSSG
+
 export const getStaticProps: GetStaticProps = async () => {
   const apolloClient = initializeApollo()
   const { data } = await apolloClient.query<GetUsersQuery>({
