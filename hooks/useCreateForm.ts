bbd9b83c39706b3a158e16bc4ff556132fbf9bee
daichi-sato-design <daichisato.design@gmail.com>
2021-10-12 20:44:12 +0900

    customHookファイルを作成

---
+++ hooks/useCreateForm.ts
@@ -0,0 +1,55 @@
+import { useState, useCallback, ChangeEvent, FormEvent } from 'react'
+import { useMutation } from '@apollo/client'
+import { CREATE_USER } from '../queries/queries'
+import { CreateUserMutation } from '../types/generated/graphql'
+
+export const useCreateForm = () => {
+  const [text, setText] = useState('')
+  const [username, setUsername] = useState('')
+  const [insert_users_one] = useMutation<CreateUserMutation>(CREATE_USER, {
+    update(cache, { data: { insert_users_one } }) {
+      const cacheId = cache.identify(insert_users_one)
+      cache.modify({
+        fields: {
+          users(existingUser, { toReference }) {
+            return [toReference(cacheId), ...existingUser]
+          },
+        },
+      })
+    },
+  })
+  const handleTextChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value)
+  }, [])
+  const usernameChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value)
+  }, [])
+  const printMsg = useCallback(() => {
+    console.log('Hello')
+  }, [])
+  const handleSubmit = useCallback(
+    async (e: FormEvent<HTMLFormElement>) => {
+      e.preventDefault()
+      try {
+        await insert_users_one({
+          variables: {
+            name: username,
+          },
+        })
+      } catch (err) {
+        alert(err.message)
+      }
+      setUsername('')
+    },
+    [username]
+  )
+
+  return {
+    text,
+    handleTextChange,
+    username,
+    usernameChange,
+    printMsg,
+    handleSubmit,
+  }
+}
