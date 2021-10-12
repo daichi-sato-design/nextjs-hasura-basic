f51f6174c46d189b578937ac2207e1fb164bc9ad
daichi-sato-design <daichisato.design@gmail.com>
2021-10-12 21:27:49 +0900

    getUsersでユーザー一覧が支障なく表示されてるかテスト

---
+++ __test__/HasuraMain.test.tsx
@@ -0,0 +1,32 @@
+import { render, screen, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event'
+import { getPage, initTestHelpers } from 'next-page-tester'
+import { setupServer } from 'msw/node'
+import { handlers } from '../mock/handlers'
+
+initTestHelpers()
+const server = setupServer(...handlers)
+beforeAll(() => {
+  server.listen()
+})
+afterEach(() => {
+  server.resetHandlers()
+  cleanup()
+})
+afterAll(() => {
+  server.close()
+})
+
+describe('Hasura Fatch Test Cases', () => {
+  it('Should render the list of users by useQuery', async () => {
+    const { page } = await getPage({
+      route: '/hasura-main',
+    })
+    render(page)
+    expect(await screen.getByText('Hasura main page')).toBeInTheDocument()
+    expect(await screen.findByText('Test user 1')).toBeInTheDocument()
+    expect(await screen.findByText('Test user 2')).toBeInTheDocument()
+    expect(await screen.findByText('Test user 3')).toBeInTheDocument()
+  })
+})
