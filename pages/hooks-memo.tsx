9a629631ff64afebb67b1f0e12c6917098bf6c2f
daichi-sato-design <daichisato.design@gmail.com>
2021-10-12 20:43:44 +0900

    customHook+memoページの生成

---
+++ pages/hooks-memo.tsx
@@ -0,0 +1,13 @@
+import { VFC } from 'react'
+import { Layout } from '../components/Layout'
+import { CreateUser } from '../components/createUser'
+
+const HooksMemo: VFC = () => {
+  return (
+    <Layout title="Hooks memo">
+      <CreateUser />
+    </Layout>
+  )
+}
+
+export default HooksMemo
